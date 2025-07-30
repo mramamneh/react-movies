c81449675b1fcf94992b9fbe655940c6ac8975f4
Mahmoud Al Ramamneh <ramamneh@gmail.com>
2025-07-31 00:36:45 +0300

    disable updating search count|appwrite

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ const App = () => {
       setMovieList(data.results || []);
 
       if(query && data.results.length > 0) {
-        await updateSearchCount(query, data.results[0]);
+        // await updateSearchCount(query, data.results[0]);
       }
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
--- src/appwrite.js
+++ src/appwrite.js
@@ -5,11 +5,12 @@ const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
-const client = new Client()
+/*const client = new Client()
   .setEndpoint('https://fra.cloud.appwrite.io/v1')
   .setProject(PROJECT_ID)
 
 const database = new Databases(client);
+*/
 
 export const updateSearchCount = async (searchTerm, movie) => {
   // 1. Use Appwrite SDK to check if the search term exists in the database
